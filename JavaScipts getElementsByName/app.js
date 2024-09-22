c6b0eec570608c3c35c50c53ad831512b37c6457
Neel <neelarmstrong33@gmail.com>
2024-09-22 23:56:20 +0530

    Code upto getElementsByName Method uses in DOM Object.

---
+++ JavaScipts getElementsByName/app.js
@@ -0,0 +1,11 @@
+let btn = document.getElementById('btnRate');
+let output = document.getElementById('output');
+
+btn.addEventListener('click', () => {
+    let rates = document.getElementsByName('rate');
+    rates.forEach((rate) => {
+        if (rate.checked) {
+            output.innerText = `You selected: ${rate.value}`;
+        }
+    });
+});
